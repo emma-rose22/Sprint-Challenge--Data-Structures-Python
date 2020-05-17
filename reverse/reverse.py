04780057cc471ade7ee7be9c3131726ebe5569f6
Emma <strainwatch3@gmail.com>
2020-05-17 19:35:14 -0400

    problem solved

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,17 +39,26 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        #set node as previous
-        if prev is not None:
-            temp = prev
-            prev = node.get_next()
-            node = temp
-            self.reverse_list(node, prev)
-        else:
-            #if self.head.get_next():
-            prev = node
-            node = node.get_next()
-            #return self.reverse_list(node, prev)
+        # if prev is not None:
+        #     temp = prev
+        #     prev = node.get_next()
+        #     node = temp
+        #     self.reverse_list(node, prev)
+        while node:
+            x = node
+            next_one = node.get_next()
+
+            node.next_node = prev
+            prev = x
+            node = next_one
+
+        self.head = prev
+
+        node, prev, node.next_node = node.get_next(), node, prev
+        self.head = prev
+
+            #update the pointer
+            #traverse the list
 
 #node needs to point at next
 #
@@ -69,14 +78,11 @@ class LinkedList:
 #             node = x
 #             #return self.reverse_list(node, prev)
 
-
-
-            
-
 ll = LinkedList()
 ll.add_to_head(1)
 ll.add_to_head(2)
 ll.add_to_head(3)
 ll.add_to_head(4)
-
-print(ll.reverse_list(ll.head, None))
+ll.add_to_head(5)
+ll.reverse_list(ll.head, None)
+print(ll.head.value)
